d6efea019c5625afc9528e3f764ac0b85f766577
JetShen <fernandoarielthielelepe@gmail.com>
2023-11-16 21:30:44 -0300

    Add missing books and update book display and styles

--- src/components/buscarlibro.tsx
+++ src/components/buscarlibro.tsx
@@ -8,61 +8,61 @@ const books = [
       category: 'Ficción',
       imageUrl: 'url_de_la_imagen_1.jpg',
     },
-    // {
-    //   title: 'Libro 2',
-    //   category: 'No ficción',
-    //   imageUrl: 'url_de_la_imagen_2.jpg',
-    // },
-    // {
-    //     title: 'Libro 3',
-    //     category: 'No ficción',
-    //     imageUrl: 'url_de_la_imagen_3.jpg',
-    // },
-    // {
-    //     title: 'Libro 4',
-    //     category: 'No ficción',
-    //     imageUrl: 'url_de_la_imagen_4.jpg',
-    //   },
-    // {
-    //     title: 'Libro 5',
-    //     category: 'No ficción',
-    //     imageUrl: 'url_de_la_imagen_5.jpg',
-    // },
-    // {
-    //     title: 'Libro 6',
-    //     category: 'No ficción',
-    //     imageUrl: 'url_de_la_imagen_6.jpg',
-    // },
-    // {
-    //     title: 'Libro 7',
-    //     category: 'No ficción',
-    //     imageUrl: 'url_de_la_imagen_7.jpg',
-    // },
-    // {
-    //     title: 'Libro 8',
-    //     category: 'No ficción',
-    //     imageUrl: 'url_de_la_imagen_8.jpg',
-    // },
-    // {
-    //     title: 'Libro 9',
-    //     category: 'No ficción',
-    //     imageUrl: 'url_de_la_imagen_9.jpg',
-    // },
-    // {
-    //     title: 'Libro 10',
-    //     category: 'No ficción',
-    //     imageUrl: 'url_de_la_imagen_10.jpg',
-    // },
-    // {
-    //     title: 'Libro 7',
-    //     category: 'No ficción',
-    //     imageUrl: 'url_de_la_imagen_7.jpg',
-    // },
-    // {
-    //     title: 'Libro 8',
-    //     category: 'No ficción',
-    //     imageUrl: 'url_de_la_imagen_8.jpg',
-    // },
+    {
+      title: 'Libro 2',
+      category: 'No ficción',
+      imageUrl: 'url_de_la_imagen_2.jpg',
+    },
+    {
+        title: 'Libro 3',
+        category: 'No ficción',
+        imageUrl: 'url_de_la_imagen_3.jpg',
+    },
+    {
+        title: 'Libro 4',
+        category: 'No ficción',
+        imageUrl: 'url_de_la_imagen_4.jpg',
+      },
+    {
+        title: 'Libro 5',
+        category: 'No ficción',
+        imageUrl: 'url_de_la_imagen_5.jpg',
+    },
+    {
+        title: 'Libro 6',
+        category: 'No ficción',
+        imageUrl: 'url_de_la_imagen_6.jpg',
+    },
+    {
+        title: 'Libro 7',
+        category: 'No ficción',
+        imageUrl: 'url_de_la_imagen_7.jpg',
+    },
+    {
+        title: 'Libro 8',
+        category: 'No ficción',
+        imageUrl: 'url_de_la_imagen_8.jpg',
+    },
+    {
+        title: 'Libro 9',
+        category: 'No ficción',
+        imageUrl: 'url_de_la_imagen_9.jpg',
+    },
+    {
+        title: 'Libro 10',
+        category: 'No ficción',
+        imageUrl: 'url_de_la_imagen_10.jpg',
+    },
+    {
+        title: 'Libro 7',
+        category: 'No ficción',
+        imageUrl: 'url_de_la_imagen_7.jpg',
+    },
+    {
+        title: 'Libro 8',
+        category: 'No ficción',
+        imageUrl: 'url_de_la_imagen_8.jpg',
+    },
     // {
     //     title: 'Libro 9',
     //     category: 'No ficción',
@@ -101,20 +101,22 @@ function Buscarlibro(){
 
     return(
         <Box>
-            <article className="book-box">
-            {books.map((book, index) => (
-                <Book
-                key={index}
-                title={book.title}
-                category={book.category}
-                imageUrl={book.imageUrl}
-                />
-            ))}
-            </article>
-            <aside className="search-box">
+            <div className="book-box">
+                <ul className="book-list">
+                    {books.map((book, index) => (
+                        <Book
+                        key={index}
+                        title={book.title}
+                        category={book.category}
+                        imageUrl={book.imageUrl}
+                        />
+                    ))}
+                </ul>
+            </div>
+            <div className="search-box">
                 <input type="text" name="query" id="query" className="Query" placeholder="Escriba el nombre del Libro"/>
                 <div className="category"></div>
-            </aside>
+            </div>
         </Box>
     );
 }
