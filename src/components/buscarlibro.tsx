b24e5e4717fd6381580234a84c7234a1a8ed9388
JetShen <fernandoarielthielelepe@gmail.com>
2023-11-27 00:58:30 -0300

    Refactor import statement in buscarlibro.tsx

--- src/components/buscarlibro.tsx
+++ src/components/buscarlibro.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Box from "./box";
 import Book from "./book";
 import Modal from './modal'; 
