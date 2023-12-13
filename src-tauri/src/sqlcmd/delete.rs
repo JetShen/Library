bb8b11da6768e989022655b35659eb0284965cd9
JetShen <fernandoarielthielelepe@gmail.com>
2023-12-12 21:18:03 -0300

    Add delete functionality for loans

--- src-tauri/src/sqlcmd/delete.rs
+++ src-tauri/src/sqlcmd/delete.rs
@@ -1,3 +1,13 @@
-pub fn delete(){
-    println!("delete");
+use super::create::conn;
+
+
+#[tauri::command]
+pub fn removeloan(idprestamo: i32) -> Result<(), String>{
+    let conn = conn();
+    println!("idPrestamo: {}", idprestamo);
+    conn.execute(
+        "DELETE FROM Prestamo WHERE idPrestamo = ?1",
+        [idprestamo],
+    ).unwrap();
+    Ok(())
 }
